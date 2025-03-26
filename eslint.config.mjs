a29d0a2d8a3913d0fe927ed121995726c1a8186a
Tiago <tiagojacintodev@gmail.com>
2025-03-26 17:17:03 +0000

    chore: setup configurations and dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import personal from '@tiagojacinto/eslint-config';
+import { config } from 'typescript-eslint';
+
+export default config(
+	{
+		ignores: ['node_modules/', 'main.js'],
+	},
+	personal.configs.recommended({
+		extensions: {
+			withProjectService: true,
+		},
+		plugins: {
+			languages: {
+				typescript: true,
+			},
+		},
+	}),
+	{
+		files: ['**/*.ts'],
+		extends: [personal.configs.node],
+		languageOptions: {
+			parserOptions: {
+				sourceType: 'module',
+			},
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+			'@typescript-eslint/ban-ts-comment': 'off',
+			'no-prototype-builtins': 'off',
+			'@typescript-eslint/no-empty-function': 'off',
+			'sonarjs/constructor-for-side-effects': 'off',
+			'sonarjs/no-invalid-await': 'off',
+		},
+	},
+);
